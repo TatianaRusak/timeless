46f66709370952681d8fe835c4f89f57b1179fda
Tatiana Rusak <tatianarusak85@gmail.com>
2023-07-01 23:48:24 +0300

    add not-scroll when burger open

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,12 +1,13 @@
 const burger = document.querySelector('.burger');
 const asideMenu = document.querySelector('.header__menu-wrapper');
 const logoHeader = document.querySelector('.logo-block_header');
-const navLinks =document.querySelectorAll('.navigation__link')
+const navLinks = document.querySelectorAll('.navigation__link');
 
 function toggleMenu() {
     burger.classList.toggle('aside-menu-open');
     asideMenu.classList.toggle('aside-menu-open');
     logoHeader.classList.toggle('aside-menu-open');
+    document.querySelector('body').classList.toggle('not-scroll');
 }
 
 
@@ -14,6 +15,7 @@ function closeMenu() {
     burger.classList.remove('aside-menu-open');
     asideMenu.classList.remove('aside-menu-open');
     logoHeader.classList.remove('aside-menu-open');
+    document.querySelector('body').classList.remove('not-scroll');
 }
 
 
